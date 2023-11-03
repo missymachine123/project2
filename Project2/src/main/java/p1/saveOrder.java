d855c5a7afa5b2f4e8b36d5d7a6b931bf2ae12c7
missymachine123 <arianams2049@gmail.com>
2023-11-02 20:57:05 -0600

    fixed issue with saveOrder

--- Project2/src/main/java/p1/saveOrder.java
+++ Project2/src/main/java/p1/saveOrder.java
@@ -1,5 +1,4 @@
 package p1;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -13,29 +12,28 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 /**
  * Servlet implementation class saveOrder
  */
+@WebServlet("/saveOrderr")
 public class saveOrder extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public saveOrder() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+	       
+	    /**
+	     * @see HttpServlet#HttpServlet()
+	     */
+	    public saveOrder() {
+	        super();
+	        // TODO Auto-generated constructor stub
+	    }
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	*/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.println("Order Processing");
+		out.println("order Processing");
 		out.println(request.getParameter("customerID"));
 		//out.println("update order");
 		int x,y=0;
@@ -43,32 +41,32 @@ public class saveOrder extends HttpServlet {
 		HttpSession session=request.getSession();
 		ArrayList<row> shoppingCart=(ArrayList<p1.row>) session.getAttribute("sCart");
 		if(shoppingCart==null)
-			out.println("<p>Empty Shop Cart!</p>");
+		out.println("<p>Empty Shop Cart!</p>");
 		else
 		{
-			//connection stuff
-			Connection mycon2=null;
-			//Statement s=null;
-			//int c=1;
+		//connection stuff
+		Connection mycon2=null;
+		//Statement s=null;
+		//int c=1;
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			mycon2 = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1","root","12345");
-			PreparedStatement insertSQL = mycon2.prepareStatement("insert into order values(?,?,?,? );");
-			//y=s.executeUpdate("Insert into order values ('s','s',3);");
-			for(int i=0; i<shoppingCart.size(); i++) {
-				insertSQL.setString(1,request.getParameter("customerID").toString()); 
-				insertSQL.setString(2,shoppingCart.get(i).getProductID());
-				  
-				int qtyOrder=shoppingCart.get(i).getQty();
-				insertSQL.setInt(3, qtyOrder);
-				insertSQL.setString(4, "Ordered");
-				x=insertSQL.executeUpdate();
-				  
-				if(x>1) {
-					y++;
-				}
-			}
-			mycon2.close();
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		mycon2 = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1","root","12345");
+		PreparedStatement insertSQL = mycon2.prepareStatement("insert into Orders values(?,?,?,?);");
+		//y=s.executeUpdate("Insert into order values ('s','s',3);");
+		for(int i=0; i<shoppingCart.size(); i++) {
+		insertSQL.setString(1,request.getParameter("customerID").toString()); 
+		insertSQL.setString(2,shoppingCart.get(i).getProductID());
+		  
+		int qtyOrder=shoppingCart.get(i).getQty();
+		insertSQL.setInt(3, qtyOrder);
+		insertSQL.setString(4,"Ordered"); 
+		x=insertSQL.executeUpdate();
+		  
+		if(x>0) {
+		y++;
+		}
+		}
+		mycon2.close();
 		} catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -81,12 +79,13 @@ public class saveOrder extends HttpServlet {
 		}
 		out.println("Successfull saving "+y+"records");
 		}
-		}
-		/**
-		* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-		*/
-		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-		}
+	}
+	/**
+	* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	doGet(request, response);
+	}
 }
+
